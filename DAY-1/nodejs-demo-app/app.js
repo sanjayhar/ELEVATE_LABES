f7063cd30e28963a0454068624ec647ddf1e82ca
Sanjaykumar S <sanjaiharish0402@gmail.com>
2025-08-04 11:55:05 +0530

    Update app.js

--- DAY-1/nodejs-demo-app/app.js
+++ DAY-1/nodejs-demo-app/app.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const app = express();
 
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js Docker CI/CD Pipeline!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
